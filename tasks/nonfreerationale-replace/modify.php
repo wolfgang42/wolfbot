ddd5bd31cb0148b721aa89eee88e86da18e23353
wolfgang42 <wolfgang42@git>
2012-11-19 18:53:46 -0500

    Fix merge conflict with master

--- tasks/nonfreerationale-replace/modify.php
+++ tasks/nonfreerationale-replace/modify.php
@@ -7,22 +7,23 @@ $changedPages=new SerialStoreArray('nonfreerationale-replace','changedPages',arr
 
 $i=0;
 foreach ($checkedPages->getData() as $page => $changeInfo) {
-	$i++;
 	if (isset($changedPages->$page)) continue; # already did this one
+	$i++;
+	echo "* $i ";
+	if ($i > 50) {echo "Reached 50 edits!"; return;} // Approved for 50 edits only
 	$pageContents=$wikipedia->getpage($page);
 	if ($wikipedia->nobots($page,'WolfBot',$pageContents)) {
 		$newPageContents=preg_replace('/\{\{[Nn]on-free media rationale/','{{Non-free use rationale',$pageContents);
 		if ($newPageContents != $pageContents) {
 			#TODO uncomment this next to actually run the bot
-			#$wikipedia->edit($page,$newPageContents,'Replaced old {{Non-free media rationale}} with {{Non-free use rationale}}');
+			$wikipedia->edit($page,$newPageContents,'Replaced old {{Non-free media rationale}} with {{Non-free use rationale}}');
 			$changedPages->$page=time();
-			echo "* [[$page]] OK\n";
-			echo $newPageContents;die();
+			echo "[[$page]] OK\n";
 			sleep (10); # Be polite
 		} else {
-			echo "* [[$page]] Error: Null edit\n";
+			echo "[[$page]] Error: Null edit\n";
 		}
 	} else {
-		echo " * [[$page]] Error: Denied\n";
+		echo "[[$page]] Error: Denied\n";
 	}
 }
